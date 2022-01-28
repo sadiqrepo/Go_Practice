fea023bf0f9d1735093e5105f4038d831bc6fab7
sadiqrepo <sadiq.n11@gmail.com>
2022-01-29 02:27:17 +0530

    loops

---
+++ loops/fordeclarations.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+
+	k := 1
+	for ; k <=10; k++ {
+		fmt.Printf("%v",k)
+	}
+	fmt.Println()
+
+	k = 1
+	for k <=5 {
+		fmt.Printf("%v",k)
+		k++
+	}
+	fmt.Println()
+
+	for k := 2; ;k++{
+		if k > 5{
+			break
+		}
+		fmt.Printf("%v",k)
+	}
+	fmt.Println()
+
+	for k:=6; k <=10; k++ {
+		fmt.Printf("%v",k)
+	}
+	fmt.Println()
+}
